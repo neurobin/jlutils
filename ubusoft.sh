d98d1f1d1e3e60b2164be60d060548a2165c1467
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-08-23 10:13:44 +0600

    update

---
+++ ubusoft.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+sudo apt install  build-essential geany gedit gparted qalculate qbittorrent git tlp
+
+#Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt install google-chrome-stable
+
+#unetbootin
+sudo add-apt-repository ppa:gezakovacs/ppa -y
+sudo apt install unetbootin
+
+#smplayer
+sudo add-apt-repository ppa:rvm/smplayer -y
+sudo apt install smplayer
+
+#rnm, shc, oraji
+sudo add-apt-repository ppa:neurobin/ppa -y
+sudo apt install rnm shc oraji
+
