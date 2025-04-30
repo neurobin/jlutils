90b6ef3a70b4275db6e49bc2dca814b4880f5137
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2025-04-30 17:00:41 +0600

    update

--- audioClean.sh
+++ audioClean.sh
@@ -2,8 +2,8 @@
 
 in="$1"
 out="$2"
-volume="${3:-1.10}"
-if [ -z "$in" ] || [ -z "$out" ]; then
+volume="${3:-10}"
+if [ -z "$in" ]; then
     echo "Usage: $0 <input_file> <output_file> <volume>"
     exit 1
 fi
@@ -22,6 +22,15 @@ if [ -f "$out" ]; then
         exit 0
     fi
 fi
+
+# clean $out by trimming it from space and dots
+out=$(echo "$out" | tr -d '[:space:]' | tr -d '.')
+
+# if out is not set it to <in>_out.extension
+if [ -z "$out" ]; then
+    out="${in%.*}_out.${in##*.}"
+fi
+
 # Check if ffmpeg is installed
 if ! command -v ffmpeg &> /dev/null; then
     echo "ffmpeg could not be found. Please install it first."
@@ -31,5 +40,6 @@ fi
 echo "Processing file: $in"
 echo "Output file: $out"
 echo "Volume: $volume"
+exit
 
 ffmpeg -i "$in" -af "highpass=f=300,asendcmd=0.0 afftdn sn start,asendcmd=1.5 afftdn sn stop,afftdn=nf=-20,dialoguenhance,lowpass=f=3000,volume=$volume" "$out"
